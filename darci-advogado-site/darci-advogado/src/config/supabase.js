d43acaa9ab57c71b7cb015f48a045fd13b713cf6
Kalzeracode <polemicacs@gmail.com>
2025-09-01 18:15:56 -0300

    Update supabase.js

--- darci-advogado-site/darci-advogado/src/config/supabase.js
+++ darci-advogado-site/darci-advogado/src/config/supabase.js
@@ -2,10 +2,10 @@ const SUPABASE_URL = 'https://sibdsejxpjgdlpdzcgej.supabase.co'
 const SUPABASE_BUCKET = 'darci-site'
 
 export const supabaseImages = {
-  logo: `${SUPABASE_URL}/storage/v1/object/public/${SUPABASE_BUCKET}/logo/logo-darci.jpg`,
+  logo: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/logo/logo-darci.jpg`,
   darciProfile: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/profile/darci-profile`,
   darciPhoto: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/profile/darci-photo.png`,
-  justiceBackground: `${SUPABASE_URL}/storage/v1/object/public/${SUPABASE_BUCKET}/backgrounds/background-justice.png`,
+  justiceBackground: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/backgrounds/background-justice.png`,
   article1: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/articles/articles01.png`,
   article2: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/articles/articles01.png`,
   article3: `https://sibdsejxpjgdlpdzcgej.supabase.co/storage/v1/object/public/darci-site/darci-site/articles/articles01.png`,
